b3c4d68971e39ab363689b147c3fbb24eaef1c4c
Jack Collinson <phantekx@gmail.com>
2019-10-14 11:44:47 +1300

    Initial commit of empty c# templates/code

---
+++ Cipher Generator/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Cipher_Generator
+{
+    class Ciphers
+    {
+
+    }
+
+    class Cipher_Generator
+    {       
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+}
